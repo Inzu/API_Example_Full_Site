ee539fb03cb594e9d7e658a531a20bd6022b2f01
Inzudotnet <Laptop@lewis-macbook.local>
2017-11-15 23:52:03 +0000

    Update

--- lib/core/functions.php
+++ lib/core/functions.php
@@ -1,12 +1,13 @@
 <?php
 
-function INZU_GET($end_point, $args, $return = false){
+function INZU_GET($end_point, $args = false, $return = false){
 
-$url = API_BASE.API_VERSION."/".$end_point."?".http_build_query($args);
+$url = API_BASE.API_VERSION."/".$end_point."?";
+
+if ( $args ) $url .= http_build_query($args);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 curl_setopt($ch, CURLOPT_USERPWD, API_KEY . ":" . API_PASS);
@@ -31,11 +32,45 @@ curl_close($ch);
 }
 
 
+
+function INZU_POST($end_point, $args, $return = false){
+
+$url = API_BASE.API_VERSION."/".$end_point;
+echo $url;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($args));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+curl_setopt($ch, CURLOPT_USERPWD, API_KEY . ":" . API_PASS);
+$output = curl_exec($ch);
+
+curl_close($ch);
+
+	if ( !$return ) {
+		
+	return json_decode($output);
+	
+	} else if ( $return == "raw" ) {
+	
+	return $output;	
+		
+	} else if ( $return == "echo" ) {
+		
+	echo $output;	
+		
+	}	
+
+}
+
+
+
 function site_map( ){
 	
 	///Create the website navigation based on your Inzu site map, this is optional.
 	
-	$inzu = INZU_GET("/general/sitemap");
+	$inzu = INZU_GET("general/sitemap");
 
 	$navigation = '<ul>';
 
